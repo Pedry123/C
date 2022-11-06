39c3873dc7a27f307f27fb44b1ca4d28ef01ec66
Pedry123 <105668982+Pedry123@users.noreply.github.com>
2022-11-05 22:07:17 -0400

    alocação dinâmica de memória

--- alura/c3/fogefoge.c
+++ alura/c3/fogefoge.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+char** mapa;
+int linhas, colunas;
 int main() {
     // matriz de 5 x 10
     //matrizes são ponteiros que apontam para uma lista de ponteiros, que apontam para endereços de memória
     // ponteiro de ponteiros: int** copia = numeros;
-    char mapa[5][10+1]; //por causa do \0
+     //por causa do \0
 
 
 
@@ -14,6 +17,22 @@ int main() {
         printf("Erro na leitura do mapa\n");
         exit(1);
     }
+
+fscanf(f, "%d %d", &linhas, &colunas);
+printf("linhas %d colunas %d\n", linhas, colunas);
+
+//////////
+mapa = malloc(sizeof(char*) * linhas);
+for(int i = 0; i < linhas; i++) {
+    mapa[i] = malloc(sizeof(char) * (colunas + 1)); //+1 pelo /0
+}
+//////////
+
+//////////////////alocação dinâmica de memória
+ //aloca quantos bytes vai usar: manualmente
+//pra alocar em um array, multiplica pelo número de elementos o sizeof(tipo)
+
+//////////////////
 
     for(int i = 0; i < 5; i++) {
         fscanf(f, "%s", mapa[i]);
@@ -32,4 +51,8 @@ int main() {
     */
 
     fclose(f); //lembrar de fechar o arquivo
+    for(int i = 0; i < linhas; i++) {
+        free(mapa[i]);
+    }
+    free(mapa);
 }
